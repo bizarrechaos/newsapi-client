545c6e1c2d91e6464dfed43bba87faece2c9caca
bizarrechaos <bcarter63@gmail.com>
2016-09-25 00:45:26 -0500

    wortha  shot

--- newsapi-client/newsapilib/output.py
+++ newsapi-client/newsapilib/output.py
@@ -1,5 +1,6 @@
 import calendar
 import json
+import yaml
 
 from datetime import datetime
 from itertools import izip_longest
@@ -7,10 +8,6 @@ from itertools import izip_longest
 
 def jprint(jsondoc):
     print json.dumps(jsondoc, sort_keys=True, indent=2, separators=(',', ': '), ensure_ascii=False).encode('utf-8', 'replace')
-
-
-def uni_json_to_utf8_dict(jsondoc):
-    return json.dumps(jsondoc, separators=(',', ': '), ensure_ascii=False).encode('utf-8', 'replace')
 
 
 def printSrcs(lst):
@@ -28,9 +25,10 @@ def printCats(cats):
 def printArts(arts):
     if arts:
         for a in arts:
-            ad = uni_json_to_utf8_dict(a)
+            jprint(a)
+            print yaml.safe_load(a)
             # dt = datetime.strptime(a['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
             # dayname = calendar.day_name[datetime.date(dt).weekday()]
             # monthname = calendar.month_name[dt.month]
             # ad['publishedAt'] = 'at {}:{} on {}, {} {}, {}'.format(dt.hour, dt.minute, dayname, monthname, dt.day, dt.year)
-            print '{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**ad)
+            #print '{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**ad)
