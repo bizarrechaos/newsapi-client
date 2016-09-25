e403809af59fde5362e51fb7d24cf7672877f680
bizarrechaos <bcarter63@gmail.com>
2016-09-25 01:28:53 -0500

    retry

--- newsapi-client/news.py
+++ newsapi-client/news.py
@@ -3,13 +3,23 @@
 """news.py
 
 Usage:
-    news [-a <apikey>] get ((sources [<category>...]|articles (category <category>...|source <source>...) [-s <sortby>])|categories)
+    news [-a <apikey>] get (categories|(sources [<category>...]|articles (source <source>...|category <category>...) [-s <sortby>]))
 
 Options:
     -a <apikey>, --apikey <apikey>    Use the provided apikey.
     -s <sortby>, --sort <sortby>      Sort the articles returned by (top, latest, popular) [default: top].
     -h, --help                        Show this page.
     -v, --version                     Show the application version.
+
+Example Usage:
+    news get categories                         Get all categories.
+    news get sources                            Get all sources.
+    news get sources general                    Get all sources classified as general.
+    news get sources general music              Get all sources classified as general, and music.
+    news get articles source cnn                Get all articles from cnn.
+    news get articles source cnn cnbc           Get all articles from cnn, and cnbc.
+    news get articles category general          Get all articles from all sources classified as general.
+    news get articles category general music    Get all articles from all sources classified as general, and music.
 """
 
 import newsapilib
