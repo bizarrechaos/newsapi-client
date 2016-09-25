df4fdd5977a91c33e227c22a6fddc7f2e3df8bd7
bizarrechaos <bcarter63@gmail.com>
2016-09-25 00:51:54 -0500

    h

--- newsapi-client/newsapilib/output.py
+++ newsapi-client/newsapilib/output.py
@@ -1,13 +1,40 @@
 import calendar
 import json
-import yaml
 
 from datetime import datetime
 from itertools import izip_longest
 
 
-def jprint(jsondoc):
-    print json.dumps(jsondoc, sort_keys=True, indent=2, separators=(',', ': '), ensure_ascii=False).encode('utf-8', 'replace')
+def json_load_byteified(file_handle):
+    return _byteify(
+        json.load(file_handle, object_hook=_byteify),
+        ignore_dicts=True
+    )
+
+
+def json_loads_byteified(json_text):
+    return _byteify(
+        json.loads(json_text, object_hook=_byteify),
+        ignore_dicts=True
+    )
+
+
+def _byteify(data, ignore_dicts = False):
+    # if this is a unicode string, return its string representation
+    if isinstance(data, unicode):
+        return data.encode('utf-8')
+    # if this is a list of values, return list of byteified values
+    if isinstance(data, list):
+        return [ _byteify(item, ignore_dicts=True) for item in data ]
+    # if this is a dictionary, return dictionary of byteified keys and values
+    # but only if we haven't already byteified it
+    if isinstance(data, dict) and not ignore_dicts:
+        return {
+            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+            for key, value in data.iteritems()
+        }
+    # if it's anything else, return it in its original form
+    return data
 
 
 def printSrcs(lst):
@@ -25,9 +52,7 @@ def printCats(cats):
 def printArts(arts):
     if arts:
         for a in arts:
-            jprint(a)
-            print a
-            print yaml.safe_load(a)
+            print json_loads_bytefield(a)
             # dt = datetime.strptime(a['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
             # dayname = calendar.day_name[datetime.date(dt).weekday()]
             # monthname = calendar.month_name[dt.month]
