665afe08d98aea1daf62e32b636dbfe2df72b5a3
bizarrechaos <bcarter63@gmail.com>
2016-09-24 23:21:23 -0500

    output fix

--- newsapi-client/newsapilib/output.py
+++ newsapi-client/newsapilib/output.py
@@ -1,5 +1,7 @@
 import json
 
+from itertools import izip_longest
+
 
 def jprint(jsondoc):
     print json.dumps(jsondoc, sort_keys=True, indent=2, separators=(',', ': '))
@@ -7,8 +9,7 @@ def jprint(jsondoc):
 
 def printSrcs(lst):
     if lst:
-        print lst
-        for a,b,c in zip(lst[::3],lst[1::3],lst[2::3]):
+        for a,b,c in izip_longest(lst[::3],lst[1::3],lst[2::3],fillvalue=' '):
             print '%-30s%-30s%s' % (a,b,c)
 
 
