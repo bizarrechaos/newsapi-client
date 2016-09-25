d1ecb5bcd5487b8746b0d4f49b9b141154a5aaf5
bizarrechaos <bcarter63@gmail.com>
2016-09-25 00:58:08 -0500

    u

--- newsapi-client/newsapilib/output.py
+++ newsapi-client/newsapilib/output.py
@@ -28,4 +28,4 @@ def printArts(arts):
             dayname = calendar.day_name[datetime.date(dt).weekday()]
             monthname = calendar.month_name[dt.month]
             a['publishedAt'] = 'at {}:{} on {}, {} {}, {}'.format(dt.hour, dt.minute, dayname, monthname, dt.day, dt.year)
-            print '{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**a)
+            print u'{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**a)
