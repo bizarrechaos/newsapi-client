4dc2cb264a767c5044ab9ae8f15f2107aa87a1b6
bizarrechaos <bcarter63@gmail.com>
2016-09-25 00:56:01 -0500

    t

--- newsapi-client/newsapilib/output.py
+++ newsapi-client/newsapilib/output.py
@@ -5,36 +5,8 @@ from datetime import datetime
 from itertools import izip_longest
 
 
-def json_load_byteified(file_handle):
-    return _byteify(
-        json.load(file_handle, object_hook=_byteify),
-        ignore_dicts=True
-    )
-
-
-def json_loads_byteified(json_text):
-    return _byteify(
-        json.loads(json_text, object_hook=_byteify),
-        ignore_dicts=True
-    )
-
-
-def _byteify(data, ignore_dicts = False):
-    # if this is a unicode string, return its string representation
-    if isinstance(data, unicode):
-        return data.encode('utf-8')
-    # if this is a list of values, return list of byteified values
-    if isinstance(data, list):
-        return [ _byteify(item, ignore_dicts=True) for item in data ]
-    # if this is a dictionary, return dictionary of byteified keys and values
-    # but only if we haven't already byteified it
-    if isinstance(data, dict) and not ignore_dicts:
-        return {
-            _byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
-            for key, value in data.iteritems()
-        }
-    # if it's anything else, return it in its original form
-    return data
+def jprint(jsondoc):
+    print json.dumps(jsondoc, sort_keys=True, indent=2, separators=(',', ': '), ensure_ascii=False).encode('utf-8', 'replace')
 
 
 def printSrcs(lst):
@@ -52,9 +24,8 @@ def printCats(cats):
 def printArts(arts):
     if arts:
         for a in arts:
-            print json_loads_byteified(a)
-            # dt = datetime.strptime(a['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
-            # dayname = calendar.day_name[datetime.date(dt).weekday()]
-            # monthname = calendar.month_name[dt.month]
-            # ad['publishedAt'] = 'at {}:{} on {}, {} {}, {}'.format(dt.hour, dt.minute, dayname, monthname, dt.day, dt.year)
-            #print '{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**ad)
+            dt = datetime.strptime(a['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
+            dayname = calendar.day_name[datetime.date(dt).weekday()]
+            monthname = calendar.month_name[dt.month]
+            ad['publishedAt'] = 'at {}:{} on {}, {} {}, {}'.format(dt.hour, dt.minute, dayname, monthname, dt.day, dt.year)
+            print '{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**ad)
