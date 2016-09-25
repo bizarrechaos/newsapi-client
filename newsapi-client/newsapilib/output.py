a88b8d8b3634e50c79696eb4a773def4e4a0d7d0
bizarrechaos <bcarter63@gmail.com>
2016-09-25 00:25:32 -0500

    fix?

--- newsapi-client/newsapilib/output.py
+++ newsapi-client/newsapilib/output.py
@@ -30,6 +30,6 @@ def printArts(arts):
                 dayname = calendar.day_name[datetime.date(dt).weekday()]
                 monthname = calendar.month_name[dt.month]
                 a['publishedAt'] = 'at {}:{} on {}, {} {}, {}'.format(dt.hour, dt.minute, dayname, monthname, dt.day, dt.year)
-                print '{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**a)
+                print '{title}\nby {author} {publishedAt}\n{description}\n{url}'.format(**a.encode('utf-8', 'replace'))
             except:
                 jprint(a)
