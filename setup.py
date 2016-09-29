0759bbe0f7d9381e94379534328d6fe9d3f31a0c
Brandon Carter <brandon.carter@rackspace.com>
2016-09-28 20:17:52 -0500

    test setup

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     packages=['news'],
     license='Apache License Version 2.0',
     description='newsapi.org command line client and wrapper',
-    long_description=open('README.txt').read(),
     install_requires=['docopt==0.6.2', 'requests==2.11.1'],
     url='https://github.com/bizarrechaos/newsapi-client',
     entry_points='''
