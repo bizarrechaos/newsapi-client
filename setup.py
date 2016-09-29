ee4e5ae48ca0611b6ded774b9790db4c6f424531
Brandon Carter <brandon.carter@rackspace.com>
2016-09-28 20:17:07 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
-from pip.req import parse_requirements
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('./requirements.txt')
-
-# reqs is a list of requirement
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='newsapi-client',
@@ -15,7 +9,7 @@ setup(
     license='Apache License Version 2.0',
     description='newsapi.org command line client and wrapper',
     long_description=open('README.txt').read(),
-    install_requires=reqs,
+    install_requires=['docopt==0.6.2', 'requests==2.11.1'],
     url='https://github.com/bizarrechaos/newsapi-client',
     entry_points='''
         [console_scripts]
